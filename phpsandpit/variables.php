0034989c5ea9650d09edf575cf6d6452a2703fdd
James Beake <jamesbeake@gmail.com>
2017-10-02 14:31:31 +1000

    variables + docker compose tweaks

---
+++ phpsandpit/variables.php
@@ -0,0 +1,28 @@
+<?
+    // comment
+    # comment
+    /*
+        comment
+        comment
+    */
+
+
+    $output = 'Hello World';
+    $num1 = 4;
+    $num2 = 10;
+    $sum = $num1 + $num2;
+
+    $string1 = 'Hello';
+    $string2 = 'World';
+
+
+    $greeting = $string1 .' '. $string2 . '!';
+    $greeting2 = "$string1 $string2";
+
+    $string3 = "They're Here";
+
+    $float1 = 4.4;
+    $bool1 = true;
+
+    echo $string3;
+?>
