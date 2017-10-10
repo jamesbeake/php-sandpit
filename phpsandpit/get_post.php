82d6773fce5c12073b24c1b91b1f73973dc43235
James Beake <jamesbeake@gmail.com>
2017-10-10 19:38:02 +1000

    get_post edits

--- phpsandpit/get_post.php
+++ phpsandpit/get_post.php
@@ -1,15 +1,17 @@
 <?php
-    // if(isset($_GET['name'])){
-    //     //print_r($_GET);
-    //     //$name = htmlentities($_GET['name']);
-    //     //echo $name;
-    // }
-
-    if(isset($_POST['name'])){
-        print_r($_POST);
-        $name = htmlentities($_POST['name']);
+    if(isset($_GET['name'])){
+        //print_r($_GET);
+        $name = htmlentities($_GET['name']);
         echo $name;
     }
+
+    // if(isset($_POST['name'])){
+    //     print_r($_POST);
+    //     $name = htmlentities($_POST['name']);
+    //     echo $name;
+    // }
+
+    // echo $_SERVER['QUERY_STRING'];
 
     // if(isset($_REQUEST['name'])){
     // // print_r($_REQUEST);
@@ -27,16 +29,27 @@
     <title>FORM processing</title>
 </head>
 <body>
-    <form action="get_post.php" method="POST">
+    <form action="get_post.php" method="GET">
         <div>
             <label for="firstname">Name</label><br>
             <input type="text" name="name" id="firstname">
-        </div>    
+        </div>
         <div>
             <label for="email">Email</label><br>
             <input type="email" name="email" id="email">
         </div>
         <input type="submit" value="Submit">
     </form>
+    <ul>
+      <li>
+        <a href="get_post.php?name=Brad">Brad's Profile</a>
+      </li>
+      <li>
+        <a href="get_post.php?name=Steve">Steve's Profile</a>
+      </li>
+    </ul>
+
+    <h1><?php echo "{$name}'s  Profile" ?></h1>
+
 </body>
 </html>
